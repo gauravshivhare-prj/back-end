4dd6b8f4ab1179083419dc1ac2574d955ecaa13a
Gaurav Shivhare <workgshiv@gmail.com>
2026-01-07 20:33:21 +0530

    Day 2 backend work

---
+++ Day-2/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("hello yaaroo")
+})
+
+app.get("/about",(req,res)=>{
+    res.send("mei about hunnn")
+})
+
+app.get("/home",(req,res)=>{
+    res.send("mein home huuu ")
+})
+
+app.listen(3001);
